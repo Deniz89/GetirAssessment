bc5ef37b28d725e50d7cb7d5aca61181514a829d
Deniz89 <ozandozel@gmail.com>
2020-08-22 15:49:35 +0300

    Port configuration changed to use .env value.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const routes = require('./routes/index.js')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
