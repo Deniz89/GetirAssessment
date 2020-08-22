74fa4b99d0a3e3f353b109736b5b8336f88b1b43
Deniz89 <ozandozel@gmail.com>
2020-08-22 16:37:42 +0300

    cont'd

--- limiters/index.js
+++ limiters/index.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const fetchRecords = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 300, // Max 300 requests
+  max: 1200, // Max 1200 requests
   message: "Too many fetch record requests have been received. Wait for 5 minutes."
 })
 
